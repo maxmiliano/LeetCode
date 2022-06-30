c1eefa318766241d78680d2c9b0933618cf5d949
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-06-30 16:10:39 -0300

    Time: 799 ms (12.38%), Space: 211.3 MB (95.72%) - LeetHub

--- 1-two-sum/1-two-sum.rb
+++ 1-two-sum/1-two-sum.rb
@@ -2,7 +2,6 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-  
   (0..(nums.size - 1)).each do |i|
     pair_index = nums.find_index(target - nums[i])
     return [i, pair_index] unless (pair_index.nil? || i == pair_index)
