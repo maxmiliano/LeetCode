47f28372a3a2b73e1dd3ccdd796841906167529b
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-01 16:20:47 -0300

    Time: 171 ms (15.75%), Space: 213.8 MB (62.65%) - LeetHub

--- 704-binary-search/704-binary-search.rb
+++ 704-binary-search/704-binary-search.rb
@@ -4,13 +4,10 @@
 def search(nums, target)
   left = 0 
   right = nums.length - 1
-  
   while left <= right
     i = (left + right) / 2
-    
     offset = ((right - left) / 2) 
     offset = 1 if offset == 0
-    
     if nums[i] == target
       return i
     elsif target > nums[i]
@@ -19,7 +16,5 @@ def search(nums, target)
       right -= offset
     end
   end
-  
   return -1
-  
 end
