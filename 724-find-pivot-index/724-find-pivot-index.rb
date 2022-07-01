10bbeecab91f58fef18a3a3437e8a04bb9d35503
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-01 11:40:57 -0300

    Time: 249 ms (32.11%), Space: 212.2 MB (72.48%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.rb
@@ -0,0 +1,29 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def pivot_index(nums)
+  
+  total_sum = nums.sum
+  left_sum, right_sum = 0, 0
+  
+  p total_sum
+  
+  nums.each_with_index do |n, i|
+    if (i == 0) 
+      left_sum = 0
+      right_sum = total_sum - n
+    elsif (i == nums.length - 1)
+      left_sum = total_sum - n
+      right_sum = 0
+    else
+      left_sum += nums[i - 1]
+      right_sum -= n
+    end
+    
+    p [left_sum, right_sum]
+    
+    return i if (right_sum == left_sum)
+  end
+  
+  return -1
+  
+end
