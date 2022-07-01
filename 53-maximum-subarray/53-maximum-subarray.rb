25400aa9d7af9d3cd4f0c29384a0cfe641211a6e
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-01 14:33:00 -0300

    Time: 311 ms (14.42%), Space: 219.1 MB (50.35%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.rb
+++ 53-maximum-subarray/53-maximum-subarray.rb
@@ -1,18 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_sub_array(nums)
-  sub_arrays = []
-  
   max_sum = - (10 ** 4)
   sum = 0
-  
   nums.each do |n|
     sum = (n > sum + n) ? n : sum + n
     max_sum = [sum, max_sum].max
-    p [sum, n, max_sum]
   end
-
-  max_sum
-  
+  max_sum 
 end
-
