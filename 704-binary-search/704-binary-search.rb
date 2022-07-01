692f76bc2cccbbb48b5f5f04adcdfe9e78f0fbd3
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-01 16:19:48 -0300

    Time: 162 ms (23.28%), Space: 213.9 MB (40.03%) - LeetHub

--- 704-binary-search/704-binary-search.rb
+++ 704-binary-search/704-binary-search.rb
@@ -7,12 +7,16 @@ def search(nums, target)
   
   while left <= right
     i = (left + right) / 2
+    
+    offset = ((right - left) / 2) 
+    offset = 1 if offset == 0
+    
     if nums[i] == target
       return i
     elsif target > nums[i]
-      left += 1
+      left += offset
     else
-      right -= 1
+      right -= offset
     end
   end
   
