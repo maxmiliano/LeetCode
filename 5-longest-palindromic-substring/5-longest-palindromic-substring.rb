ff03047283f09409fd9b725f6085eac1aa5bd3dc
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-02 20:47:56 -0300

    Time: 2770 ms (5.24%), Space: 211.1 MB (58.08%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.rb
@@ -0,0 +1,41 @@
+# @param {String} s
+# @return {String}
+def longest_palindrome(s)
+  n = s.length
+  
+  return s if n < 2
+  
+  max_length = 1
+  max_start = 0
+  left, right = 0, 0
+  
+  0.upto(n - 1) do |i|
+    left = i - 1
+    right = i + 1
+    
+    while(right < n && s[right] == s[i]) 
+      right += 1
+    end
+    
+    while(left >= 0 && s[left] == s[i])
+      left -= 1
+    end
+    
+    while(left >= 0 && right < n && s[left] == s[right])
+      left -= 1
+      right += 1
+    end
+    
+    length = right - left - 1
+    # p [i, length, max_length, s[left + 1, length]]
+    
+    if (max_length < length)
+      max_length = length
+      max_start = left + 1
+      # p "current max: #{s[max_start, max_length]}"
+    end
+  end
+  
+  s[max_start, max_length]
+  
+end
