d6e879c1c1083bf4ca35f8fcb53a0427dff2a13a
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-01 23:52:19 -0300

    Time: 100 ms (80.25%), Space: 210.9 MB (87.65%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.rb
+++ 392-is-subsequence/392-is-subsequence.rb
@@ -2,20 +2,14 @@
 # @param {String} t
 # @return {Boolean}
 def is_subsequence(s, t)
-  
   tj = []
   i = 0
   previous_i = -1
-
   0.upto(t.length - 1) do |j|
     if (s[i] == t[j])
       tj.push(j) if s[i] == t[j]
       i += 1
     end
-    p [i, j, tj]
   end
-
-  
   return (tj.length == s.length) && (tj == tj.sort)
-  
 end
