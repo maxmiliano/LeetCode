7e99dae43965d0975ebd64a1748e1c2df470485b
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-03 02:15:36 -0300

    Time: 241 ms (33.33%), Space: 218.4 MB (33.33%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.rb
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.rb
@@ -10,17 +10,17 @@ def max_area(h, w, horizontal_cuts, vertical_cuts)
   hcs = horizontal_cuts.sort << h
   vcs = vertical_cuts.sort << w
   
-  hslices = []
+  hslices = [] # size of each horizontal slice
   hcs.each_with_index do |el, i|
     i == 0 ? hslices.push(el) : hslices.push(el - (hcs[i - 1]))
   end
 
-  vslices = []
+  vslices = [] # size of each vertical slice
   vcs.each_with_index do |el, i|
     i == 0 ? vslices.push(el) : vslices.push(el -(vcs[i - 1]))
   end
 
-  p [hslices, vslices]
+  # p [hslices, vslices]
   
   return (hslices.max * vslices.max).modulo((10 ** 9) + 7)
   
