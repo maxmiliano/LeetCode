725c6e4b09449e71b762f5fffb7c1135f96fa5e4
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-04 18:53:14 -0300

    Time: 140 ms (33.96%), Space: 211.6 MB (8.49%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.rb
@@ -0,0 +1,25 @@
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
+def intersect(nums1, nums2)
+
+  # key = number | value == how many times number is found in arry
+  hash_1 = Hash.new(0)
+  hash_2 = Hash.new(0)
+  
+  nums1.each do |n1|
+    hash_1[n1] += 1
+  end
+  
+  nums2.each do |n2|
+    hash_2[n2] += 1
+  end
+  
+  result = nums1.intersection(nums2).map do |item|
+    # p [item, hash_1[item], hash_2[item]]
+    Array.new([hash_1[item], hash_2[item]].min, item)
+  end.flatten
+  
+  result
+  
+end
