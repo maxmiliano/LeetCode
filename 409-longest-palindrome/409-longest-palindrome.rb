2fa54e690fbb48dce3e2fc9caf4f74d446944cbb
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-07 00:24:00 -0300

    Time: 144 ms (38.16%), Space: 211.2 MB (10.53%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.rb
@@ -0,0 +1,25 @@
+# @param {String} s
+# @return {Integer}
+def longest_palindrome(s)
+ 
+  return 0 if s.empty?
+  
+  chars = s.chars
+  letters = chars.tally # count each uniq letter
+
+  evens, odds = letters.partition { |k, v| v.even? }.map(&:to_h)
+  p [odds, evens]
+  
+  result = evens.values.sum # Evens letter make palindromes
+
+  # Every two chars from even counts for our longest palindrome
+  odds.values.each do |v|
+    result += (v / 2) * 2
+  end
+  
+  # Can pick any resting char from odds to put in the middle and still have a palindrome  
+  result += 1 if odds.length > 0 
+  
+  result
+  
+end
