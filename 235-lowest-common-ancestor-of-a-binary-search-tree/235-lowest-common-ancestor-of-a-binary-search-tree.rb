734e1f0d4755b8c627344a10528127222768e308
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-08 00:46:39 -0300

    Time: 132 ms (71.07%), Space: 212.3 MB (91.74%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.rb
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.rb
@@ -16,7 +16,7 @@ def lowest_common_ancestor(root, p, q)
   p_linage = find_ancestors(p, root)
   q_linage = find_ancestors(q, root)
   
-  Kernel.p [p_linage.map(&:val), q_linage.map(&:val)]
+  # Kernel.p [p_linage.map(&:val), q_linage.map(&:val)]
 
   common_val = p_linage.map(&:val) & q_linage.map(&:val)
   common_ancestors = p_linage.select{|n| common_val.include?(n.val)} + q_linage.select{|n| common_val.include?(n.val)}
