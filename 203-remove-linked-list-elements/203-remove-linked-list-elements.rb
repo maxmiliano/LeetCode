821e3340aa7c5aebb876de192f0ec6ecbea4d3f6
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-12 11:27:35 -0300

    Time: 153 ms (59.65%), Space: 212.3 MB (56.14%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.rb
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @param {Integer} val
+# @return {ListNode}
+def remove_elements(head, val)
+
+  current = head
+  prev = nil
+  
+  while(!current.nil?) do
+    if current.val == val
+      if prev.nil?
+        # removing the head (head.next will become the new head)
+        current = head = current.next
+      else
+        # we are removing intermediary node or tail
+        prev.next = current = current.next
+      end
+    else
+      # current != val, let's keep moving
+      prev = current
+      current = current.next 
+    end
+  end
+  
+  return head
+  
+end
