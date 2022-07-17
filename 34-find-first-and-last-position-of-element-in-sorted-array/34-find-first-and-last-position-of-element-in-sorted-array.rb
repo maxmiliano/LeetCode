ec2f26f792c1cca6c9bddcc8a8fb436c63d95b52
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-17 14:15:20 -0300

    Time: 119 ms (56.44%), Space: 211.5 MB (97.03%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def search_range(nums, target)
+  i = first = nums.index(target)
+  
+  return [-1, -1] unless i
+  
+  while(nums[i] == target)
+    i += 1
+  end
+  
+  return [first, i-1]
+  
+end
