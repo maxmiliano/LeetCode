9a9be5efafdc9913039299b650c98e37b5094fc5
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-07-17 09:29:17 -0300

    Time: 91 ms (83.67%), Space: 211.1 MB (46.26%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.rb
@@ -0,0 +1,39 @@
+# @param {String} digits
+# @return {String[]}
+def letter_combinations(digits)
+  map = {
+  '2' => ['a', 'b', 'c'],
+  '3' => ['d', 'e', 'f'],
+  '4' => ['g', 'h', 'i'],
+  '5' => ['j', 'k', 'l'],
+  '6' => ['m', 'n', 'o'],
+  '7' => ['p', 'q', 'r', 's'],
+  '8' => ['t', 'u', 'v'],
+  '9' => ['w', 'x', 'y', 'z'],
+  }
+  
+  cached_letter_combinations(map, digits)
+  
+end
+
+def cached_letter_combinations(map, digits)
+
+  return [] if digits.length == 0
+  return map[digits[0]] if digits.length == 1
+  
+  middle = digits.length / 2
+  
+  left = digits[0..(middle-1)]
+  right = digits[middle..(digits.length-1)]
+  
+  map[left] ||= cached_letter_combinations(map, left)
+  map[right] ||= cached_letter_combinations(map, right)
+  
+  p [digits, map]
+  
+  return map[left]
+    .product(map[right])
+    .collect { |l,r| l+r }
+  
+  # return map[digits]
+end
