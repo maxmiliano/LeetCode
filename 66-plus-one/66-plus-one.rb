e5caf6c495e2c553c6b660962b2d76c771966850
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-08-29 14:11:17 -0300

    Time: 109 ms (62.65%), Space: 210.8 MB (95.98%) - LeetHub

---
+++ 66-plus-one/66-plus-one.rb
@@ -0,0 +1,23 @@
+# @param {Integer[]} digits
+# @return {Integer[]}
+def plus_one(digits)
+  plus_one_at(digits, digits.length - 1)
+end
+
+def plus_one_at(digits, i)
+  
+  p [digits, i]
+  
+  digits[i] = digits[i] + 1
+
+  if digits[i] >= 10
+    digits[i] -= 10
+    if (i > 0)  
+      plus_one_at(digits, i - 1) 
+    else
+      digits.insert(0, 1)
+    end
+  end
+  
+  digits
+end
