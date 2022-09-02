ac173cfcc34081830262cce6fb66c5423e891c9d
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-09-01 23:35:25 -0300

    Time: 200 ms (22.75%), Space: 211.2 MB (17.99%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.rb
@@ -0,0 +1,33 @@
+# @param {Integer} num
+# @return {String}
+def int_to_roman(num)
+  roman = ""
+  
+  roman << "M" * (num / 1000)
+  num %= 1000
+  roman << "CM" * (num / 900)
+  num %= 900
+  roman << "D" * (num / 500)
+  num %= 500
+  roman << "CD" * (num / 400)
+  num %= 400
+  roman << "C" * (num / 100)
+  num %= 100
+  roman << "XC" * (num / 90)
+  num %= 90
+  roman << "L" * (num / 50)
+  num %= 50
+  roman << "XL" * (num / 40)
+  num %= 40
+  roman << "X" * (num / 10)
+  num %= 10
+  roman << "IX" * (num /9)
+  num %= 9
+  roman << "V" * (num / 5)
+  num %= 5
+  roman << "IV" * (num /4)
+  num %= 4
+  roman << "I" * num
+  
+  roman
+end
