e50c73c14b4ba6f2afa2d51be94c6fca477b8d82
Maxmiliano Franco Braga <maxmiliano@gmail.com>
2022-09-07 17:41:49 -0300

    Time: 247 ms (50.00%), Space: 231.7 MB (50.00%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.rb
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {String}
+def tree2str(root)
+  
+  return "" if root.nil?
+  
+  sol = root.val.to_s
+  
+  
+  sol << "(#{tree2str(root.left)})" unless (root.left.nil?)
+  unless (root.right.nil?)
+    sol << "()" if root.left.nil?
+    sol <<  "(#{tree2str(root.right)})"
+  end
+  
+  return sol
+  
+end
