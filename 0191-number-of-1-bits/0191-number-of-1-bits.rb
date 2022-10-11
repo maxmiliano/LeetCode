54bbba344a0efdce247c43ff3eeb1326370e5ad7
Maxmiliano F Braga <maxmiliano@gmail.com>
2022-10-11 15:36:12 -0300

    Time: 152 ms (36.00%), Space: 210.9 MB (57.00%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.rb
+++ 0191-number-of-1-bits/0191-number-of-1-bits.rb
@@ -1,5 +1,8 @@
 # @param {Integer} n, a positive integer
 # @return {Integer}
 def hamming_weight(n)
-  n.to_s(2).chars.select{|c| c == '1'}.count
+  n.to_s(2)
+    .chars
+    .select{|c| c == '1'}
+    .count
 end
